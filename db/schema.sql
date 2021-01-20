b1118d8235cca93431be9f3c1c362aadaea8e9d4
jessparnow@gmail.com <jessparnow@gmail.com>
2021-01-19 19:09:42 -0600

    adding passport authetication files

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,3 @@
 -- DROP DATABASE restaurants_db;
 CREATE DATABASE restaurants_db;
 USE restaurants_db;
-
-CREATE TABLE restaurant (
-id INT NOT NULL AUTO_INCREMENT,
-user_name VARCHAR(50) NOT NULL,
-review VARCHAR(255) NOT NULL,
-restaurant_name VARCHAR(50) NOT NULL,
-PRIMARY KEY (id)
-);
