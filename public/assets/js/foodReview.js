3e6f1b3d614f3f85b39364ad1b50e0997838c78a
jessparnow@gmail.com <jessparnow@gmail.com>
2021-01-26 14:35:32 -0600

    adding delete and edit routes

--- public/assets/js/foodReview.js
+++ public/assets/js/foodReview.js
@@ -28,4 +28,36 @@ $("#create-review").on("click", function (event) {
     // Reload the page to get the updated list
     location.reload();
   });
+  $(".editReview").on("click", function (event) {
+    let id = $(this).data("id");
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newMealState,
+    });
+
+    function updatePost(post) {
+      $.ajax({
+        method: "PUT",
+        url: "/api/posts",
+        data: post,
+      }).then(function () {
+        window.location.href = "/blog";
+      });
+    }
+    updatePost();
+  });
+
+  $(".deleteReview").on("click", function (event) {
+    let id = $(this).data("id");
+
+    // Send the DELETE request.
+    $.ajax("/api/review/" + id, {
+      type: "DELETE",
+    }).then(function () {
+      console.log("deleted review", id);
+      // Reload the page to get the updated list
+      location.reload();
+    });
+  });
 });
