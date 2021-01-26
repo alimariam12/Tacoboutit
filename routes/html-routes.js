0654aeebc7696ff780c80f1adf7b0db66afb5870
Mariam Ali <alim12@csp.edu>
2021-01-26 13:53:11 -0600

    fixed REVIEW PAGE

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -22,10 +22,20 @@ module.exports = function (app) {
     res.render("review");
   });
   app.get("/members", isAuthenticated, (req, res) => {
-    res.render("review");
+    db.Review.findAll({
+      raw: true,
+      // where: {
+      //   title: req.params.title,
+      //   body: req.params.body,
+      // }
+    }).then(function (data) {
+      const hbsObject = {
+        reviews: data,
+      };
+    res.render("review", hbsObject);
   });
-};
-
+});
+}
 // Here we've add our isAuthenticated middleware to this route.
 // If a user who is not logged in tries to access this route they will be redirected to the signup page
 //   app.get("/", function(req, res) {
