504c6a65fb253a0f5471a123f89f5b54b9e7cbf3
jessparnow@gmail.com <jessparnow@gmail.com>
2021-01-26 11:07:59 -0600

    replacing ajax call

--- public/assets/js/foodReview.js
+++ public/assets/js/foodReview.js
@@ -1,26 +1,30 @@
 $.ajax("/api/members", {
   type: "GET",
   // data: newReview
-}).then(
-  function(data) {
-    console.log(data);
+}).then(function (data) {
+  console.log(data);
+  // Reload the page to get the updated list
+  // location.reload();
+});
+
+$("#create-review").on("click", function (event) {
+  // Make sure to preventDefault on a submit event.
+  event.preventDefault();
+  console.log("clicked bby!!");
+  const titleInput = $("input#nameNine");
+  const bodyInput = $("textarea#messageNine");
+
+  let newReview = {
+    title: titleInput.val(),
+    body: bodyInput.val(),
+  };
+  console.log(newReview);
+  $.ajax("/api/review", {
+    type: "POST",
+    data: newReview,
+  }).then(function () {
+    console.log("created new review bby");
     // Reload the page to get the updated list
     location.reload();
-  })
-
-$("#create-review").on("click", function(event) {
-
-    // Make sure to preventDefault on a submit event.
-    event.preventDefault();
-console.log("clicked bby!!");
-const loginForm = $("#logMeIn");
-const titleInput = $("input#nameNine");
-const bodyInput = $("textarea#messageNine");
-
-    let newReview = {
-      title: titleInput.val(),
-      body: bodyInput.val(),
-    };
-    console.log(newReview);
-    });
-    
+  });
+});
